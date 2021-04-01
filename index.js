daf896ca183b0267433dd08fa3f9f7bb630369fc
ambarishg <ambarish.ganguly@gmail.com>
2021-04-01 12:14:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+//  get the required packages
 const util = require('util');
 const fs = require('fs');
 const TrainingApi = require("@azure/cognitiveservices-customvision-training");
 const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
 const msRest = require("@azure/ms-rest-js");
 
-const sampleDataRoot = "f:/bees/";
+const sampleDataRoot = "<your folder>";
 
-const predictionKey = "5e2245d8f8344fb581449b9d7c24b389";
+//  Configure your authentication details
+const predictionKey = "<key of the prediction resource>";
 const endPoint = "https://bees.cognitiveservices.azure.com/"
 const publishIterationName = "bees"
-const projectid = 0 
+const projectid = "<project id >" 
 
+//  Authenticate with Azure Cognitive Services
 const predictor_credentials = new msRest.ApiKeyCredentials({ inHeader: { "Prediction-key": predictionKey } });
 const predictor = new PredictionApi.PredictionAPIClient(predictor_credentials, endPoint);
 
-
-const testFile = fs.readFileSync('f:/bees/Test/040_314.png');
+//  Predict the disease of the unseen data, if not any disease , it should return healthy
+const testFile = fs.readFileSync('<your file name -  bees01.png>');
 
 (async () => {
-const results = await predictor.classifyImage("56e92ed0-e242-42bf-85c1-9a382e83b61e",
+const results = await predictor.classifyImage("projectid",
  publishIterationName, testFile);
 
 // Show results
